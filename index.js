3fe689061d8111540cb16afc908585d3b114f205
Bruno Silva Costa <futebol44@outlook.com>
2024-04-27 16:47:18 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function RolarParaDireita(){
+    var setadireita = window.document.getElementById("seta-direita")
+var leonardo = window.document.getElementById("leonardo")
+var samanta = window.document.getElementById("samanta")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("seta-esquerda")
+    leonardo.style = "display:none" 
+    bruna.style = "display:flex"
+    setadireita.style= "display:none"
+    setaesquerda.style = "display:flex"
+}
+function RolarParaEsquerda(){ 
+    var setadireita = window.document.getElementById("seta-direita")
+var leonardo = window.document.getElementById("leonardo")
+var samanta = window.document.getElementById("samanta")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("seta-esquerda")
+    var leonardo = window.document.getElementById("leonardo")
+    var bruna = window.document.getElementById("bruna")
+    leonardo.style = "display : flex"
+    bruna.style = "display: none"
+    
+}
